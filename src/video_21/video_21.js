3a103df9905248088be1e8a177ef57fdc88ae5f1
Manh <manh.vu@wisewires.com>
2025-09-26 16:28:38 +0700

    new commit

---
+++ src/video_21/video_21.js
@@ -0,0 +1,15 @@
+console.log("Video 21");
+console.error("Error");
+console.warn("Warning");
+// const name = "Manh";
+// const age = 20;
+
+let name = "Manh";
+let age = 20;
+
+
+console.log(name, age);
+console.log("My name is:", name, "Age:", age);
+
+console.log(`My name is: ${name} - Age: ${age}`);
+
